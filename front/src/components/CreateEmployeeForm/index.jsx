326aaa35f9a9a8c526f43d8523f6810c46e21638
SyDyJo <gadyamb.re@gmail.com>
2025-06-11 08:23:52 +0200

    feat: implémentation du datepicker

--- front/src/components/CreateEmployeeForm/index.jsx
+++ front/src/components/CreateEmployeeForm/index.jsx
@@ -7,50 +7,48 @@ function CreateEmployeeForm({ setModalOpen }) {
   }
 
   return (
-    <>
-      <form action="/" id="create-employee">
-        <label htmlFor="first-name">First Name</label>
-        <input type="text" id="first-name" />
+    <form action="/" id="create-employee">
+      <label htmlFor="first-name">First Name</label>
+      <input type="text" id="first-name" />
 
-        <label htmlFor="last-name">Last Name</label>
-        <input type="text" id="last-name" />
+      <label htmlFor="last-name">Last Name</label>
+      <input type="text" id="last-name" />
 
-        <label htmlFor="date-of-birth">Date of Birth</label>
-        <input id="date-of-birth" type="text" />
+      <label htmlFor="date-of-birth">Date of Birth</label>
+      <input id="date-of-birth" type="date" />
 
-        <label htmlFor="start-date">Start Date</label>
-        <input id="start-date" type="text" />
+      <label htmlFor="start-date">Start Date</label>
+      <input id="start-date" type="date" />
 
-        <fieldset className="address">
-          <legend>Address</legend>
+      <fieldset className="address">
+        <legend>Address</legend>
 
-          <label htmlFor="street">Street</label>
-          <input id="street" type="text" />
+        <label htmlFor="street">Street</label>
+        <input id="street" type="text" />
 
-          <label htmlFor="city">City</label>
-          <input id="city" type="text" />
+        <label htmlFor="city">City</label>
+        <input id="city" type="text" />
 
-          <label htmlFor="state">State</label>
-          <select name="state" id="state"></select>
+        <label htmlFor="state">State</label>
+        <select name="state" id="state"></select>
 
-          <label htmlFor="zip-code">Zip Code</label>
-          <input id="zip-code" type="number" />
-        </fieldset>
+        <label htmlFor="zip-code">Zip Code</label>
+        <input id="zip-code" type="number" />
+      </fieldset>
 
-        <label htmlFor="department">Department</label>
-        <select name="department" id="department">
-          <option>Sales</option>
-          <option>Marketing</option>
-          <option>Engineering</option>
-          <option>Human Resources</option>
-          <option>Legal</option>
-        </select>
+      <label htmlFor="department">Department</label>
+      <select name="department" id="department">
+        <option>Sales</option>
+        <option>Marketing</option>
+        <option>Engineering</option>
+        <option>Human Resources</option>
+        <option>Legal</option>
+      </select>
 
-        <button className="submit-btn" onClick={saveEmployee}>
-          Save
-        </button>
-      </form>
-    </>
+      <button className="submit-btn" onClick={saveEmployee}>
+        Save
+      </button>
+    </form>
   )
 }
 
